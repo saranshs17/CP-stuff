6c96f02f301785f36224bfc37f7ba5ea795de4d4
Saransh <114410043+saranshs17@users.noreply.github.com>
2024-03-10 17:58:36 +0530

    Update BFS.cpp

--- Graph/BFS.cpp
+++ Graph/BFS.cpp
@@ -1,84 +1,21 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-void addEdge(unordered_map<int, list<int>> &adj, int u, int v,
-             bool isdirected) {
-  // if is directed graph then isdirected == true
-  // if is undirected graph then isdirected == false
-
-  // create an edje from u to v
-  adj[u].push_back(v);
-  if (isdirected == false) {
-    adj[v].push_back(u);
-  }
-}
-void printAdjList(unordered_map<int, list<int>> &adj) {
-  for (auto i : adj) {
-    cout << i.first << "->";
-    for (auto j : i.second) {
-      cout << j << ",";
+// Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        int vis[V]={0};
+        queue<int> q;
+        vis[0]=1;
+        q.push(0);
+        vector<int> bfs;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            bfs.push_back(node);
+            for(auto x:adj[node]){
+                if(!vis[x]){
+                    vis[x]=1;
+                    q.push(x);
+                }
+            }
+        }
+        return bfs;
     }
-    cout << '\n';
-  }
-}
-
-void bfs(unordered_map<int, list<int>> &adj, unordered_map<int, bool> &visited,
-         vector<int> &ans, int node) {
-  queue<int> q;
-  q.push(node);
-  visited[node] = 1;
-
-  while (!q.empty()) {
-    int frontNode = q.front();
-    q.pop();
-
-    ans.push_back(frontNode);
-
-    for (auto i : adj[frontNode]) {
-      if (!visited[i]) {
-        q.push(i);
-        visited[i] = 1;
-      }
-    }
-  }
-}
-
-vector<int> bfsTraversal(int n, unordered_map<int, list<int>> &adj) {
-  unordered_map<int, bool> visited;
-  vector<int> ans;
-
-  for (int i = 0; i < n; ++i) {
-    if (!visited[i]) {
-      bfs(adj, visited, ans, i);
-    }
-  }
-  return ans;
-}
-
-int main() {
-  int n;
-  cout << "Enter the number of nodes" << '\n';
-  cin >> n;
-  cout << "Enter the number of edges" << '\n';
-  int m;
-  cin >> m;
-  cout << "Enter 0 to make undirected graph or 1 to make directed graph"
-       << '\n';
-  bool f;
-  cin >> f;
-  unordered_map<int, list<int>> adj;
-  cout << "Enter u and v " << '\n';
-  for (int i = 0; i < m; ++i) {
-    int u, v;
-    cin >> u >> v;
-    addEdge(adj, u, v, f);
-  }
-  cout << "Adjacency List: " << '\n';
-  printAdjList(adj);
-  vector<int> bfs = bfsTraversal(n, adj);
-  cout << "BFS Traversal: ";
-  for (auto x : bfs) {
-    cout << x << " ";
-  }
-  cout << '\n';
-}
