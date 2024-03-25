e5974f267db99dcac6bf6943bc9eb221ed029597
Saransh <114410043+saranshs17@users.noreply.github.com>
2024-03-25 11:29:52 +0530

    Update DFS.cpp

--- Graph/DFS.cpp
+++ Graph/DFS.cpp
@@ -14,3 +14,26 @@ void dfs(int node,vector<int>& list,vector<int> adj[],vector<int> &vis){
         dfs(0,list,adj,vis);
         return list;
     }
+/*
+
+
+void dfs(int vertex,int parent)
+{
+      //operations to be performed after entering a node.	
+	for(auto e:graph[vertex])
+	{
+		if(e==parent)
+		{
+			continue;
+		}
+                 //operations to be performed before going to child node.
+                 // while going down
+		dfs(e,vertex);
+
+            //operation to be performed after completing dfs from a child node.
+            //while coming up
+      }
+     //operation to be performed before exiting a node.
+}
+
+*/
