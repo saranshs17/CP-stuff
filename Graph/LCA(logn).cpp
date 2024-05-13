16601156cba56ba56b38952e1a8ffa1d924d58b3
Saransh <114410043+saranshs17@users.noreply.github.com>
2024-05-13 18:18:56 +0530

    Update LCA(logn).cpp

--- Graph/LCA(logn).cpp
+++ Graph/LCA(logn).cpp
@@ -4,24 +4,24 @@ int up[NN][20];
 int level[NN];
 void binaryLift(int u,int par){
     up[u][0]=par;
-    if(par!=-1)level[u]=level[par]+1;
     for(int i=1;i<=19;++i){
-        if(up[u][i-1]!=-1){
-            up[u][i]=up[up[u][i-1]][i-1];
-        }else up[u][i]=-1;
+        up[u][i]=up[up[u][i-1]][i-1];
     }
     for(auto x:adj[u]){
         if(x!=par){
+            level[x]=level[u]+1;
             binaryLift(x,u);
         }
     }
 }
 int kthparent(int u,int k){
-    if(u==-1||k==0)return u;
     for(int i=19;i>=0;--i){
         int jump=(1<<i);
-        if(k>=jump)return kthparent(up[u][i],k-jump);
+        if(k&jump){
+            u=up[u][i];
+        }
     }
+    return up[u][0];
 }
 int LCA(int u,int v){
     if(level[u]<level[v])swap(u,v);
@@ -54,8 +54,8 @@ int32_t main() {
             adj[i].pb(x);
             adj[x].pb(i);
         }
-        level[1]=1;
-        binaryLift(1,-1);
+        level[1]=0;
+        binaryLift(1,1);
         while(q--){
             int u,v;cin>>u>>v;
             cout<<LCA(u,v)<<'\n';
