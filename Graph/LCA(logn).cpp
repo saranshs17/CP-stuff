4872d3e493838e598f758769fbbd077332137fb8
Saransh <114410043+saranshs17@users.noreply.github.com>
2024-05-18 23:56:48 +0530

    Update LCA(logn).cpp

--- Graph/LCA(logn).cpp
+++ Graph/LCA(logn).cpp
@@ -4,24 +4,24 @@ int up[NN][20];
 int level[NN];
 void binaryLift(int u,int par){
     up[u][0]=par;
+    if(par!=-1)level[u]=level[par]+1;
     for(int i=1;i<=19;++i){
-        up[u][i]=up[up[u][i-1]][i-1];
+        if(up[u][i-1]!=-1){
+            up[u][i]=up[up[u][i-1]][i-1];
+        }else up[u][i]=-1;
     }
     for(auto x:adj[u]){
         if(x!=par){
-            level[x]=level[u]+1;
             binaryLift(x,u);
         }
     }
 }
 int kthparent(int u,int k){
+    if(u==-1||k==0)return u;
     for(int i=19;i>=0;--i){
         int jump=(1<<i);
-        if(k&jump){
-            u=up[u][i];
-        }
+        if(k>=jump)return kthparent(up[u][i],k-jump);
     }
-    return up[u][0];
 }
 int LCA(int u,int v){
     if(level[u]<level[v])swap(u,v);
@@ -36,9 +36,6 @@ int LCA(int u,int v){
         }
     }
     return up[u][0]; // here u and v not equal but thier par i.e. LCA is equal
-}
-int dist(int u,int v){                          
-    return level[u]+level[v]-2*level[LCA(u,v)];                     // calculate distance btw u and v in log n 
 }
 int32_t main() {
     //sieve();
@@ -55,7 +52,7 @@ int32_t main() {
             adj[x].pb(i);
         }
         level[1]=0;
-        binaryLift(1,1);
+        binaryLift(1,-1);
         while(q--){
             int u,v;cin>>u>>v;
             cout<<LCA(u,v)<<'\n';
