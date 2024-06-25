0f2fc144e2221780bed719e39850f8fac9e0dd10
Saransh Shivhare <114410043+saranshs17@users.noreply.github.com>
2024-06-25 21:59:02 +0530

    Update ct no. of  numbers btw L , R such that even dig should be on even pos and odd digit on odd pos in choosen number.cpp
    Google Kickstart

--- Digit DP/ct no. of  numbers btw L , R such that even dig should be on even pos and odd digit on odd pos in choosen number.cpp
+++ Digit DP/ct no. of  numbers btw L , R such that even dig should be on even pos and odd digit on odd pos in choosen number.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 // #include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/tree_policy.hpp> 
 using namespace std;
 using namespace __gnu_pbds;
 #define sped ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); 
