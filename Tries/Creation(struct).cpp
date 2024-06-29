deaf1487d9c688fd145acf6dd9bfe9b3bb7a1341
Saransh Shivhare <114410043+saranshs17@users.noreply.github.com>
2024-06-29 20:55:45 +0530

    Create Creation(struct).cpp

---
+++ Tries/Creation(struct).cpp
@@ -0,0 +1,65 @@
+/*
+    Your Trie object will be instantiated and called as such:
+    Trie* obj = new Trie();
+    obj->insert(word);
+    bool check2 = obj->search(word);
+    bool check3 = obj->startsWith(prefix);
+ */
+
+struct Node{
+    Node * links[26];
+    bool flag=false;
+    bool containsKey(char c){
+        return links[c-'a']!=NULL;
+    }
+    void put(char c,Node * node){
+        links[c-'a']=node;
+    }
+    Node * next(char c){
+        return links[c-'a'];
+    }
+    bool isEnd(){
+        return flag==true;
+    }
+};
+class Trie {
+private: Node * root;
+public:
+
+    /** Initialize your data structure here. */
+    Trie() {
+        root=new Node();
+    }
+
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        Node * node=root;
+        for(int i=0;i<word.size();++i){
+            if(!node->containsKey(word[i])){
+                node->put(word[i],new Node());
+            }
+            node=node->next(word[i]);
+        }
+        node->flag=true;
+    }
+
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        Node * node=root;
+        for(int i=0;i<word.size();++i){
+            if(!node->containsKey(word[i])) return false;
+            node=node->next(word[i]);
+        }
+        return node->isEnd();
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+        Node * node=root;
+        for(int i=0;i<prefix.size();++i){
+            if(!node->containsKey(prefix[i])) return false;
+            node=node->next(prefix[i]);
+        }
+        return true;
+    }
+};
